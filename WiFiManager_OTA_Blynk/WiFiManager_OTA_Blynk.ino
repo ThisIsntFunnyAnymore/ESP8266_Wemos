dd94e9d03fdb08a437d8ebf96ac1619dce29dce6
ThisIsntFunnyAnymore <jrkinsfather@gmail.com>
2017-12-20 17:54:47 -0700

    Got the main WifiManager_OTA_Blynk sketch up and running. Note it is a little finicky; the sketch crashes if the JSON object requests a parameter that is not there. You need to format SPIFFS or delete the /config.json file.
    I also created the WifiManager_Cleanup sketch to clear out a device and
    put it in OTA mode. Note I have not tested any of the OTA functions yet.

--- WiFiManager_OTA_Blynk/WiFiManager_OTA_Blynk.ino
+++ WiFiManager_OTA_Blynk/WiFiManager_OTA_Blynk.ino
@@ -1,3 +1,35 @@
+// 20171220:15:20 compile but errors out. Start small
+/******************************************************************************
+   WifiManager + OTA + Blynk by ThisIsntFunnyAnymore
+   Version 0.0.1, 2017 December 20
+
+   This is a prototype function to combine the following services:
+      - tzapu's WifiManager
+      - Arduino OTA updating
+      - Blynk
+      - Filesystem & JSON
+
+   In addition to the combination of the above, it also adds:
+      - Basic diagnostic outputs in Blynk via Blynk timer
+      - Blynk terminal outputs (serial connection not required for most functions)
+      - Reset from Blynk (kinda pointless outside of testing, I know)
+
+   The advantage of this function is that it already combines many of the core
+   technologies I want to include in my major project. It also hides the personal
+   information (SSID, Password, private server name, Blynk token) from public
+   sources like GitHub and Blynk community.
+
+   I will not acknowledge everybody's work, so let's just say I'm standing on the
+   shoulders of giants.
+
+   Note: Currently the following pins are in use in the sketch:
+      - V0:   Terminal widget
+      - V124: safety for WiFiManager reset
+      - V125: trigger for WiFiManager reset
+      - V126: WiFi Signal Strength
+      - V127: Uptime in Seconds
+ * ****************************************************************************/
+
 #include <FS.h>                   //this needs to be first, or it all crashes and burns...
 
 #include <ESP8266WiFi.h>          //https://github.com/esp8266/Arduino
@@ -8,14 +40,22 @@
 #include <WiFiManager.h>          //https://github.com/tzapu/WiFiManager
 
 #include <ArduinoJson.h>          //https://github.com/bblanchon/ArduinoJson
+#include <ArduinoOTA.h>           //https://github.com/esp8266/Arduino/tree/master/libraries/ArduinoOTA
+#include <BlynkSimpleEsp8266.h>   //https://github.com/blynkkk/blynk-library
 
-//define your default values here, if there are different values in config.json, they are overwritten.
-char mqtt_server[40];
-char mqtt_port[6] = "8080";
+//define your default values here, if there are different values in config.json,
+//they are overwritten.
+char mqtt_server[40] = "blynk-cloud.com";
+char mqtt_port[6] = "8442";
 char blynk_token[34] = "YOUR_BLYNK_TOKEN";
+char esp_hostname[20] = "esp8266_Device";
 
 //flag for saving data
 bool shouldSaveConfig = false;
+
+//Blynk definitions
+BlynkTimer timer;
+WidgetTerminal terminal(V0);
 
 //callback notifying us of the need to save config
 void saveConfigCallback () {
@@ -23,13 +63,79 @@ void saveConfigCallback () {
   shouldSaveConfig = true;
 }
 
+// A simple function to show the Blynk is running. Sends WiFi signal & uptime
+void myDeviceStats () {
+  Blynk.virtualWrite(V126, WiFi.RSSI());
+  Blynk.virtualWrite(V127, millis() / 1000);
+}
+
+
+int safePin = LOW, triggerPin = LOW;
+
+// A reset function for the WiFiManager & JSON data
+// The button tied to V124 must be selected when the reset button is pushed for
+// anything to happen
+void deviceFallback() {
+
+  //this may be a bit too much. I think I want to start a basic on-demand to change 
+  //SSID and Password only (why would I change token et. al.?)
+  if (safePin == HIGH && triggerPin == HIGH) {
+    terminal.println(F("Connect to 'AutoConnectAP' and sign in at 192.168.4.1"));
+    terminal.println(F("Have your Blynk server/port/token ready, because we're"));
+    terminal.print(F("clearing wifi settings and resetting device in  "));
+    //simple countdown to give you just enough time to read the terminal.
+    for (int i = 3; i >= 0; i--) {
+      terminal.print(i);
+      terminal.print("...");
+      delay(1000);
+    }
+    Blynk.virtualWrite(V0, false);
+    //local instance of WiFiManager
+    WiFiManager wifiManager;
+    wifiManager.resetSettings();
+    SPIFFS.format();
+    ESP.restart();
+
+  } else {
+    terminal.println(F("Cannot reset WiFi with Safety on."));
+  }
+}
+
+//Set safePin
+BLYNK_WRITE(V124) {
+  safePin = param.asInt();
+}
+
+//Fire trigger
+BLYNK_WRITE(V125) {
+  triggerPin = param.asInt();
+  if (triggerPin == HIGH) {
+    deviceFallback();
+  }
+}
+
+//This function just cleans up the terminal once connected.
+BLYNK_CONNECTED() {
+  char BlynkConnectString[] = "Connection to Blynk established";
+  Serial.println(BlynkConnectString);
+  // Clear terminal
+  for (int i = 1; i < 10; i++) {
+    terminal.println("");
+  }
+  terminal.println(BlynkConnectString);
+  terminal.print("\n");
+}
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(115200);
   Serial.println();
 
-  //clean FS, for testing
+  /*clean FS, for testing
+    JK - You need to format the SPIFFS to remove /config.json when
+    creating or deleting custom parameters, otherwise the sketch will
+    crash. Use with wifiManager.resetSettings() below.
+    Trust me, I know this. */
   //SPIFFS.format();
 
   //read configuration from FS json
@@ -57,6 +163,7 @@ void setup() {
           strcpy(mqtt_server, json["mqtt_server"]);
           strcpy(mqtt_port, json["mqtt_port"]);
           strcpy(blynk_token, json["blynk_token"]);
+          strcpy(esp_hostname, json["esp_hostname"]);
 
         } else {
           Serial.println("failed to load json config");
@@ -68,14 +175,13 @@ void setup() {
   }
   //end read
 
-
-
   // The extra parameters to be configured (can be either global or just in the setup)
   // After connecting, parameter.getValue() will get you the configured value
   // id/name placeholder/prompt default length
   WiFiManagerParameter custom_mqtt_server("server", "mqtt server", mqtt_server, 40);
   WiFiManagerParameter custom_mqtt_port("port", "mqtt port", mqtt_port, 5);
-  WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 32);
+  WiFiManagerParameter custom_blynk_token("blynk", "blynk token", blynk_token, 34);
+  WiFiManagerParameter custom_esp_hostname("device", "device hostname", esp_hostname, 20);
 
   //WiFiManager
   //Local intialization. Once its business is done, there is no need to keep it around
@@ -85,21 +191,21 @@ void setup() {
   wifiManager.setSaveConfigCallback(saveConfigCallback);
 
   //set static ip
-  wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
-  
+  // wifiManager.setSTAStaticIPConfig(IPAddress(10,0,1,99), IPAddress(10,0,1,1), IPAddress(255,255,255,0));
+
   //add all your parameters here
   wifiManager.addParameter(&custom_mqtt_server);
   wifiManager.addParameter(&custom_mqtt_port);
   wifiManager.addParameter(&custom_blynk_token);
+  wifiManager.addParameter(&custom_esp_hostname);
 
   //reset settings - for testing
   //wifiManager.resetSettings();
-  //ESP.reset();
 
   //set minimu quality of signal so it ignores AP's under that quality
   //defaults to 8%
   //wifiManager.setMinimumSignalQuality();
-  
+
   //sets timeout until configuration portal gets turned off
   //useful to make it all retry or go to sleep
   //in seconds
@@ -124,6 +230,7 @@ void setup() {
   strcpy(mqtt_server, custom_mqtt_server.getValue());
   strcpy(mqtt_port, custom_mqtt_port.getValue());
   strcpy(blynk_token, custom_blynk_token.getValue());
+  strcpy(esp_hostname, custom_esp_hostname.getValue());
 
   //save the custom parameters to FS
   if (shouldSaveConfig) {
@@ -133,6 +240,7 @@ void setup() {
     json["mqtt_server"] = mqtt_server;
     json["mqtt_port"] = mqtt_port;
     json["blynk_token"] = blynk_token;
+    json["esp_hostname"] = esp_hostname;
 
     File configFile = SPIFFS.open("/config.json", "w");
     if (!configFile) {
@@ -148,10 +256,27 @@ void setup() {
   Serial.println("local ip");
   Serial.println(WiFi.localIP());
 
+  // Now that we have a successful WiFi connection and all of our data is
+  // saved, we can start our services.
+
+  // Set the ESP Hostname to its saved value and start OTA service
+  ArduinoOTA.setHostname(esp_hostname);
+  ArduinoOTA.begin();
+
+  // Configure and start Blynk (courtesy Costas July 2016 community.blynk.cc
+  unsigned int int_mqtt_port = atoi(mqtt_port);
+  Blynk.config(blynk_token, mqtt_server, int_mqtt_port); // in place of Blynk.begin(auth, ssid, pass)
+  Blynk.connect(); // connect with default timeout of 10 sec
+  while (!Blynk.connected()) {
+    //wait until Blynk is connected before proceeding to the loop()
+  }
+  timer.setInterval(10000L, myDeviceStats);  // get stats every 10 seconds. Fine for basic knowledge
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  Blynk.run();
+  timer.run();
+  ArduinoOTA.handle();
 
 
 }
